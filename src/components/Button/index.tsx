ed678cf36273a901d28af2afa85debc9582af1e4
Kyle Oliveiro <hello@kyle.sg>
2022-12-22 10:48:07 +0800

    feat: add hover state to Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -15,16 +15,16 @@ const Button: FC<ButtonProps> = ({
 }) => {
     const variantClasses = {
         default: 'bg-transparent rounded-2xl text-gray border-2',
-        primary: 'bg-offblack rounded-2xl text-offwhite',
+        primary: 'bg-offblack rounded-2xl text-offwhite border-2 border-black hover:text-offblack hover:bg-transparent',
     }
 
     const sizeClasses = {
         md: 'text-sm px-5 py-3',
-        lg: 'text-lg px-8 py-4',
+        lg: 'text-md px-6 py-3',
     }
 
     return (
-        <a href={href} className={`inline-flex ${variantClasses[variant]} ${sizeClasses[size]}`}>
+        <a href={href} className={`inline-flex font-700 ${variantClasses[variant]} ${sizeClasses[size]}`}>
             {children}
         </a>
     )
