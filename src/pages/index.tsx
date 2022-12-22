c033deeaae3c6c719fcd64c7a1a9520d9457a531
Kyle Oliveiro <hello@kyle.sg>
2022-12-22 11:16:33 +0800

    feat: update responsive styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,54 +12,56 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon-32x32.png" />
       </Head>
 
-      <main>
-        <div className="mb-10">
-          <div style={{
-            backgroundImage: 'url(/images/hero/image-hero-mobile.png)',
-            backgroundSize: 'cover',
-            paddingBottom: `${564 / 750 * 100}%`,
-          }} />
-        </div>
+      <main className="pb-16">
+        <div className="md:container mx-auto">
+          <div className="mb-10 max-w-xl mx-auto">
+            <div style={{
+              backgroundImage: 'url(/images/hero/image-hero-mobile.png)',
+              backgroundSize: 'cover',
+              paddingBottom: `${564 / 750 * 100}%`,
+            }} />
+          </div>
 
-        <div className="text-center px-2">
-          <header className="mb-3">
-            <h1 className="text-3xl font-700">Make remote work</h1>
-          </header>
-          <div className="mb-6 text-gray">
-            <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
-          </div>
-          <div className="mb-9">
-            <Button href="#" variant="primary" size="lg">Learn more</Button>
-          </div>
-          <div>
-            <LogoList
-              items={[
-                {
-                  title: 'Databiz',
-                  image: '/images/clients/client-databiz.svg',
-                  width: 114,
-                  height: 20,
-                },
-                {
-                  title: 'Audiophile',
-                  image: '/images/clients/client-audiophile.svg',
-                  width: 73,
-                  height: 36,
-                },
-                {
-                  title: 'Meet',
-                  image: '/images/clients/client-meet.svg',
-                  width: 90,
-                  height: 20,
-                },
-                {
-                  title: 'Maker',
-                  image: '/images/clients/client-maker.svg',
-                  width: 83,
-                  height: 24,
-                },
-              ]}
-            />
+          <div className="container mx-auto text-center px-3">
+            <header className="mb-3">
+              <h1 className="text-3xl font-700">Make remote work</h1>
+            </header>
+            <div className="mb-6 text-gray">
+              <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
+            </div>
+            <div className="mb-12">
+              <Button href="#" variant="primary" size="lg">Learn more</Button>
+            </div>
+            <div>
+              <LogoList
+                items={[
+                  {
+                    title: 'Databiz',
+                    image: '/images/clients/client-databiz.svg',
+                    width: 114,
+                    height: 20,
+                  },
+                  {
+                    title: 'Audiophile',
+                    image: '/images/clients/client-audiophile.svg',
+                    width: 73,
+                    height: 36,
+                  },
+                  {
+                    title: 'Meet',
+                    image: '/images/clients/client-meet.svg',
+                    width: 90,
+                    height: 20,
+                  },
+                  {
+                    title: 'Maker',
+                    image: '/images/clients/client-maker.svg',
+                    width: 83,
+                    height: 24,
+                  },
+                ]}
+              />
+            </div>
           </div>
         </div>
       </main>
