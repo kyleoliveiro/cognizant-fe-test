8fc6504eaf5e3d4c9906ad566b06a5139d5697e3
Kyle Oliveiro <hello@kyle.sg>
2022-12-22 10:14:22 +0800

    feat: add homepage text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -9,7 +10,19 @@ const Home: NextPage = () => {
         <meta name="description" content="Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar." />
         <link rel="icon" href="/favicon-32x32.png" />
       </Head>
+
       <main>
+        <div className="space-y-3 text-center px-2">
+          <header>
+            <h1 className="text-3xl font-700">Make remote work</h1>
+          </header>
+          <div className="text-gray">
+            <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
+          </div>
+          <div>
+            <Link href="#">Learn more</Link>
+          </div>
+        </div>
       </main>
     </>
   );
