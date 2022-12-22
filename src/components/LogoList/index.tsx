3ece4a5a9b59d24c3242a1204c2ffee09c0ba1fe
Kyle Oliveiro <hello@kyle.sg>
2022-12-22 11:10:21 +0800

    feat: add LogoList

---
+++ src/components/LogoList/index.tsx
@@ -0,0 +1,28 @@
+import { FC } from "react";
+
+interface LogoListItem {
+    title: string,
+    image: string,
+    height: number,
+    width: number,
+}
+
+interface LogoListProps {
+    items: LogoListItem[]
+}
+
+const LogoList: FC<LogoListProps> = ({
+    items,
+}) => {
+    return (
+        <ul className="grid grid-cols-4 gap-4">
+            {items.map(item => (
+                <li className="flex items-center justify-center">
+                    <img src={item.image} alt={item.title} height={item.height} width={item.width} />
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+export default LogoList
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Button from "../components/Button";
+import LogoList from "../components/LogoList";
 
 const Home: NextPage = () => {
   return (
@@ -31,7 +32,34 @@ const Home: NextPage = () => {
             <Button href="#" variant="primary" size="lg">Learn more</Button>
           </div>
           <div>
-            Logos
+            <LogoList
+              items={[
+                {
+                  title: 'Databiz',
+                  image: '/images/clients/client-databiz.svg',
+                  width: 114,
+                  height: 20,
+                },
+                {
+                  title: 'Audiophile',
+                  image: '/images/clients/client-audiophile.svg',
+                  width: 73,
+                  height: 36,
+                },
+                {
+                  title: 'Meet',
+                  image: '/images/clients/client-meet.svg',
+                  width: 90,
+                  height: 20,
+                },
+                {
+                  title: 'Maker',
+                  image: '/images/clients/client-maker.svg',
+                  width: 83,
+                  height: 24,
+                },
+              ]}
+            />
           </div>
         </div>
       </main>
