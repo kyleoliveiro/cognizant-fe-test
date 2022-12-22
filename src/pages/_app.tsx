2dae5c2da4ce77e9fbbc45ef6224156d43ad3389
Kyle Oliveiro <hello@kyle.sg>
2022-12-22 09:44:26 +0800

    feat: add epilogue webfont

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-
+import "@fontsource/epilogue/500.css"
+import "@fontsource/epilogue/700.css"
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
